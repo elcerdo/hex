99e95221ab7926f6d91d38aeccbcd889d4cddbbf
Pierre Gueth <pierre.gueth@gmail.com>
2016-06-28 17:22:01 +0200

    stress binary

---
+++ stress_uct.cpp
@@ -0,0 +1,15 @@
+#include "game.h"
+#include "player_uct.h"
+
+int main(int argc, char* argv[])
+{
+    const Board board(5);
+    PlayerUct player0(board, 1, 5, 123456789);
+    PlayerUct player1(board, 1, 5, 123456789);
+    BoardState state(board);
+
+    const int winner = run_game_loop(board, state, &player0, &player1, NULL);
+
+    return 0;
+}
+
