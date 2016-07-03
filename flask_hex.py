d31d7ee3e4541b5252b530a0347837c2628b61c5
Pierre Gueth <pierre.gueth@gmail.com>
2016-07-03 20:48:44 +0200

    flask app

--- flask_hex.py
+++ flask_hex.py
@@ -10,9 +10,15 @@ sys.path.append("build")
 import libhex_uct_ext as hex_uct
 import flask
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_folder="static")
+
+games = []
 
 @app.route("/")
 def root():
-    return flask.render_template("root.html")
+    return flask.render_template("root.html", route_name="root")
+
+@app.route("/newgame")
+def newgame():
+    return flask.render_template("newgame.html", route_name="newgame")
 
