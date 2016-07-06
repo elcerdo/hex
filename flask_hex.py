bdb4403fb3f060f7c36d4a61a4455b97f3926ea2
Pierre Gueth <pierre.gueth@gmail.com>
2016-07-06 17:51:25 +0200

    display game page

--- flask_hex.py
+++ flask_hex.py
@@ -9,15 +9,22 @@ sys.path.append("build")
 
 import libhex_uct_ext as hex_uct
 import flask
+import uuid
 
-app = flask.Flask(__name__, static_folder="static")
+app = flask.Flask(__name__)
 app.secret_key = "0oqtjWXYAv0OIz3LjBGoap4xiA/m-5IvJ"
 
-games = []
+with app.app_context():
+    flask.current_app.games = {}
 
 @app.route("/")
 def root():
-    return flask.render_template("root.html", route_name="root", games=games)
+    return flask.render_template("root.html", route_name="root", games=flask.current_app.games)
+
+@app.route("/displaygame/<uuid:game_id>")
+def displaygame(game_id):
+    game = flask.current_app.games[game_id]
+    return flask.render_template("displaygame.html", route_name="displaygame", game=game, game_id=game_id)
 
 @app.route("/newgame", methods=["GET", "POST"])
 def newgame():
@@ -28,10 +35,14 @@ def newgame():
             uct_time = float(flask.request.form["uct_time"])
             uct_prune = flask.request.form["uct_prune"] == "on"
             uct_constant = float(flask.request.form["uct_constant"])
-            print(flask.request.form)
-            print(player_name, player_is_white)
-            print(uct_time, uct_prune, uct_constant)
-            games.append((player_name, player_is_white, uct_time, uct_prune, uct_constant))
+            #print(flask.request.form)
+            #print(player_name, player_is_white)
+            #print(uct_time, uct_prune, uct_constant)
+
+            game_id = uuid.uuid1()
+            assert( game_id not in flask.current_app.games )
+            flask.current_app.games[game_id] = (player_name, player_is_white, uct_time, uct_prune, uct_constant)
+
             flask.flash("new game created", "info")
             return flask.redirect(flask.url_for('root'))
         except ValueError:
